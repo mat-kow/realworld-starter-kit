6679304f1651ac808143c5d282af5761f0075564
unknown <mateokow90@gmail.com>
2026-02-08 11:14:42 +0100

    spring project init

---
+++ src/test/java/pl/teo/realworldapp/RealworldAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.teo.realworldapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealworldAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
